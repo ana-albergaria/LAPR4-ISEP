941437fcf1e97fee32d080b02c702bea68d4aa68
Nuno Silva 1201516 <1201516@isep.ipp.pt>
2022-04-29 15:51:09 +0100

    Issue #9 - The implementation of this US is now fully completed. Need to do tests.

--- base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/agv/RegisterAGVUI.java
+++ base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/agv/RegisterAGVUI.java
@@ -3,15 +3,14 @@ package eapli.base.app.backoffice.console.presentation.agv;
 import eapli.base.infrastructure.persistence.PersistenceContext;
 import eapli.base.warehousemanagement.application.RegisterAGVController;
 import eapli.base.warehousemanagement.domain.*;
+import eapli.base.warehousemanagement.repositories.AGVRepository;
 import eapli.base.warehousemanagement.repositories.AgvDockRepository;
 import eapli.framework.domain.repositories.IntegrityViolationException;
 import eapli.framework.io.util.Console;
 import eapli.framework.presentation.console.AbstractUI;
 
 import java.io.Reader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class RegisterAGVUI extends AbstractUI {
 
@@ -22,45 +21,69 @@ public class RegisterAGVUI extends AbstractUI {
         String option = "";
         Long numDocks = Long.valueOf(0);
         AgvDockRepository dockRepository = PersistenceContext.repositories().agvDocks();
+        AGVRepository agvRepository = PersistenceContext.repositories().agvs();
         Scanner read = new Scanner(System.in);
+
+        Long agvID;
+        String autonomyStatus, taskStatus, modelID, shortDescription;
+        Double maxWeight;
+        AutonomyStatus autonomy;
+        TaskStatus task;
+
         do {
-            final Long agvID = Console.readLong("AGV ID:");
-            final String autonomyStatus = Console.readLine("Autonomy Status:");
-            final String taskStatus = Console.readLine("Task Status:");
-            final String modelID = Console.readLine("Model ID:");
-            final String shortDescription = Console.readLine("Short Description:");
-            final Double maxWeight = Console.readDouble("Max Weight:");
-            /*final String agvDockID = Console.readLine("AGV Dock ID:");
-            final Long beginSquareLength = Console.readLong("Begin Square Length:");
-            final Long beginSquareWidth = Console.readLong("Begin Square Width:");
-            final Long endSquareLength = Console.readLong("End Square Length:");
-            final Long endSquareWidth = Console.readLong("End Square Width:");
-            final Long depthSquareLength = Console.readLong("Depth Square Length:");
-            final Long depthSquareWidth = Console.readLong("Depth Square Width:");
-            final String accessibilityDir = Console.readLine("Accessibility Direction:");*/
-
-            AutonomyStatus autonomy = new AutonomyStatus(autonomyStatus);
-            TaskStatus task = new TaskStatus(taskStatus);
-            //Accessibility accessibilityDirection = new Accessibility(accessibilityDir);
-
             Map<Integer, AgvDock> dockOptions = new HashMap<>();
-            numDocks = dockRepository.count();
             AgvDock dock = null;
-            int i = 1;
 
-            System.out.println("To which dock do you want to associate the AGV?");
-            for(AgvDock agvDock : dockRepository.findAll()){
-                System.out.println(i + " - " + agvDock.getAgvDockID());
-                dockOptions.put(i, agvDock);
-                i++;
+            Iterable<AgvDock> docks = dockRepository.findAll();
+            List<AgvDock> selectedDocks = (List<AgvDock>) docks;
+            List<String> dockIDs = new LinkedList<>();
+            List<AGV> agvList = (List<AGV>) agvRepository.findAll();
+
+            for(AgvDock dockOfAGV : docks){
+                dockIDs.add(dockOfAGV.identity());
             }
 
-            final Integer selectedDock = read.nextInt();
-
-            for(Integer numberedDocks : dockOptions.keySet()){
-                if(numberedDocks.equals(selectedDock)){
-                    dock = dockOptions.get(numberedDocks);
+            for(AGV agv : agvList){
+                AgvDock theAGVDock = agv.getAgvDock();
+                String id = theAGVDock.getAgvDockID();
+                if(dockIDs.contains(id)){
+                    selectedDocks.remove(theAGVDock);
                 }
+            }
+
+            numDocks = (long) selectedDocks.size();
+
+            int i = 1;
+
+            if(!selectedDocks.isEmpty()) {
+                agvID = Console.readLong("AGV ID:");
+                autonomyStatus = Console.readLine("Autonomy Status:");
+                taskStatus = Console.readLine("Task Status:");
+                modelID = Console.readLine("Model ID:");
+                shortDescription = Console.readLine("Short Description:");
+                maxWeight = Console.readDouble("Max Weight:");
+
+                autonomy = new AutonomyStatus(autonomyStatus);
+                task = new TaskStatus(taskStatus);
+
+                System.out.println("To which dock do you want to associate the AGV?");
+                for(AgvDock agvDock : selectedDocks) {
+                    System.out.println(i + " - " + agvDock.getAgvDockID());
+                    dockOptions.put(i, agvDock);
+                    i++;
+                }
+
+                final Integer selectedDock = read.nextInt();
+
+                for (Integer numberedDocks : dockOptions.keySet()) {
+                    if (numberedDocks.equals(selectedDock)) {
+                        dock = dockOptions.get(numberedDocks);
+                        numDocks--;
+                    }
+                }
+            }else{
+                System.out.println("No more available Docks. Try again later...");
+                return false;
             }
 
             try {
@@ -70,13 +93,13 @@ public class RegisterAGVUI extends AbstractUI {
                 System.out.println("You tried to enter an AGV that already exists in the database.");
             }
 
-            if(numDocks < 0){
+            if(numDocks <= 0){
                 System.out.println("No more docks available, so it is not possible to register any more AGVs.");
             }else {
                 option = Console.readLine("Do you want to register more AGVs? \n Yes - Y | No - N");
             }
 
-        }while ((!option.equalsIgnoreCase("no") && !option.equalsIgnoreCase("n"))/* && (numDocks < 0)*/);
+        }while ((!option.equalsIgnoreCase("no") && !option.equalsIgnoreCase("n")) && (numDocks < 0));
 
         return false;
     }
--- base.core/src/main/java/eapli/base/warehousemanagement/repositories/AgvDockRepository.java
+++ base.core/src/main/java/eapli/base/warehousemanagement/repositories/AgvDockRepository.java
@@ -3,5 +3,7 @@ package eapli.base.warehousemanagement.repositories;
 import eapli.base.warehousemanagement.domain.AgvDock;
 import eapli.framework.domain.repositories.DomainRepository;
 
-public interface AgvDockRepository extends DomainRepository<Long, AgvDock> {
+public interface AgvDockRepository extends DomainRepository<String, AgvDock> {
+    @Override
+    Iterable<AgvDock> findAll();
 }
