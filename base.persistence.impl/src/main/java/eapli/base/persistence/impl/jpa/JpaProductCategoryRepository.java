569929820a4c83cc72b9a5bef2da32f5d2a11b2e
Nuno Silva 1201516 <1201516@isep.ipp.pt>
2022-05-01 15:32:59 +0100

    Issue #3 - Full implementation of this US. The .md file for this US is still pending.

--- base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaProductCategoryRepository.java
+++ base.persistence.impl/src/main/java/eapli/base/persistence/impl/jpa/JpaProductCategoryRepository.java
@@ -1,12 +1,13 @@
 package eapli.base.persistence.impl.jpa;
 
 import eapli.base.productmanagement.domain.AlphaNumericCode;
+import eapli.base.productmanagement.domain.ExtendedDescription;
+import eapli.base.productmanagement.domain.Product;
 import eapli.base.productmanagement.domain.ProductCategory;
 import eapli.base.productmanagement.repositories.ProductCategoryRepository;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import javax.persistence.TypedQuery;
+import java.util.*;
 
 public class JpaProductCategoryRepository extends BaseJpaRepositoryBase<ProductCategory, AlphaNumericCode, AlphaNumericCode>
 implements ProductCategoryRepository {
@@ -20,5 +21,22 @@ implements ProductCategoryRepository {
         final Map<String, Object> params = new HashMap<>();
         params.put("code",code);
         return matchOne("e.productcategory.alphanumericcode=:code", params);
+    }
+
+    @Override
+    public Iterable<ProductCategory> findAllCategory(){
+        final TypedQuery<Product> query = entityManager().createQuery(
+                "SELECT p FROM Product p",
+                Product.class);
+
+        Iterable<Product> products = query.getResultList();
+        List<ProductCategory> filteredByCategoryList = new LinkedList<>();
+
+        for (Product p : products){
+            if (!filteredByCategoryList.contains(p.getProductCategory())){
+                filteredByCategoryList.add(p.getProductCategory());
+            }
+        }
+        return filteredByCategoryList;
     }
 }
