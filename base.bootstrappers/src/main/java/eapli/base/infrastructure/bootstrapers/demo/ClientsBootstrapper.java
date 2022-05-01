f65abadabd655038898422d968d7826f59cf5636
martaribeiro <1201592@isep.ipp.pt>
2022-05-01 18:51:27 +0100

    Issue #8 - Added second client to bootstrap.

--- base.bootstrappers/src/main/java/eapli/base/infrastructure/bootstrapers/demo/ClientsBootstrapper.java
+++ base.bootstrappers/src/main/java/eapli/base/infrastructure/bootstrapers/demo/ClientsBootstrapper.java
@@ -11,6 +11,8 @@ import org.springframework.transaction.TransactionSystemException;
 
 import java.util.*;
 
+import static eapli.base.clientmanagement.domain.Client.Gender.MASCULINE;
+
 /**
  * @author Marta Ribeiro 1201592
  */
@@ -30,8 +32,29 @@ public class ClientsBootstrapper implements Action {
         address1.add("Rio Meão");
         address1.add("Portugal");
         addresses.add(address1);
+        List<List<String>> addresses2 = new ArrayList<>();
+        List<String> address2 = new ArrayList<>();
+        address2.add("R Germana Tânger");
+        address2.add("108");
+        address2.add("2725-121");
+        address2.add("Algueirão");
+        address2.add("Portugal");
+        addresses2.add(address2);
+        List<String> address3 = new ArrayList<>();
+        address3.add("Avenida República");
+        address3.add("22");
+        address3.add("4750-055");
+        address3.add("Granja");
+        address3.add("Portugal");
+        addresses2.add(address3);
         register("Ana", "de Castro Albergaria", "1201518@isep.ipp.pt", "+351918413784",
-                "PT999999999",addresses, null, null);
+                "PT999999999", addresses, null, null);
+        Calendar calBirthDate1 = Calendar.getInstance();
+        calBirthDate1.setTime(new Date(1970, Calendar.NOVEMBER, 15));
+        register("Manuel", "Santos Oliveira", "1201518@isep.ipp.pt", "+351918170262",
+                "PT999999999",addresses2, calBirthDate1, MASCULINE);
+        /*register("Manuel António", "Santos Oliveira", "1201518@isep.ipp.pt", "+351918170262",
+                "PT999999999", addresses2, calBirthDate1, MASCULINE);*/
         return true;
     }
 
@@ -40,11 +63,11 @@ public class ClientsBootstrapper implements Action {
                          final Calendar birthdate, final Client.Gender gender){
         try{
             controller.registerClient(firstNames, surnames, email, phoneNumber, vat, addresses, birthdate, gender);
-            LOGGER.debug(firstNames, surnames, email);
+            LOGGER.debug(firstNames, surnames, email, phoneNumber);
         } catch (final IntegrityViolationException | ConcurrencyException | TransactionSystemException e){
             // ignoring exception. assuming it is just a primary key violation
             // due to the tentative of inserting a duplicated user
-            LOGGER.warn("Assuming {} {} with email {} already exists (see trace log for details on {} {})", firstNames, surnames, email,
+            LOGGER.warn("Assuming {} {} with email {} and phone number {} already exists (see trace log for details on {} {})", firstNames, surnames, email, phoneNumber,
                     e.getClass().getSimpleName(), e.getMessage());
             LOGGER.trace(String.valueOf(e));
         }
