f2bc1b195cfaa760a51af5396a8252149c712d98
Diogo Violante <1201284@isep.ipp.pt>
2022-05-05 18:20:01 +0100

    no message

--- base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/MainMenu.java
+++ base.app.backoffice.console/src/main/java/eapli/base/app/backoffice/console/presentation/MainMenu.java
@@ -94,9 +94,8 @@ public class MainMenu extends AbstractUI {
     // WAREHOUSE EMPLOYEE
     private static final int REGISTER_AGV = 1;
     private static final int CONFIGURE_AVAILABLE_AGVS = 2;
-    private static final int INSERT_AGV_INFORMATIONS = 3;
-    private static final int AVAILABLE_AGVS_INFORMATIONS = 4;
-    private static final int SET_UP_PLANT=5;
+    private static final int INSERT_AGV_INFORMATIONS = 2;
+    private static final int SET_UP_PLANT=3;
 
     private static final String SEPARATOR_LABEL = "--------------";
 
@@ -207,7 +206,6 @@ public class MainMenu extends AbstractUI {
 
         menu.addItem(REGISTER_AGV, "Register AGV", new RegisterAGVUI()::show);
         menu.addItem(CONFIGURE_AVAILABLE_AGVS, "Configure Available AGV", new ConfigureAvailableAGVUI()::show);
-        menu.addItem(AVAILABLE_AGVS_INFORMATIONS, "Show Available AGVs Description", new ConfigureAvailableAGVUI()::show);
         menu.addItem(SET_UP_PLANT, "Set the Warehouse Plant", new SetUpPlantUI()::show);
         menu.addItem(EXIT_OPTION, RETURN_LABEL, Actions.SUCCESS);
 
