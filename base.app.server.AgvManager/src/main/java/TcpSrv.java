94a3aaa0b4090e84d77c79c9449656cfb0f47950
Nuno Silva 1201516 <1201516@isep.ipp.pt>
2022-05-20 21:12:40 +0100

    Issue #18 - Implementation in order to run the Server on Cloud.

--- base.app.server.AgvManager/src/main/java/TcpSrv.java
+++ base.app.server.AgvManager/src/main/java/TcpSrv.java
@@ -1,12 +1,12 @@
-import eapli.base.ordermanagement.domain.OrderStatus;
+/*import eapli.base.ordermanagement.domain.OrderStatus;
 import eapli.base.ordermanagement.domain.TheOrder;
 import eapli.base.warehousemanagement.application.AutomaticallyAssignOrdersToFreeAGVController;
 import eapli.base.warehousemanagement.domain.AGV;
-import eapli.base.warehousemanagement.domain.TaskStatus;
+import eapli.base.warehousemanagement.domain.TaskStatus;*/
 
 import java.io.*;
 import java.net.*;
-import java.util.List;
+//import java.util.List;
 
 class TcpSrvSum {
     static ServerSocket sock;
@@ -14,13 +14,16 @@ class TcpSrvSum {
     public static void main(String args[]) throws Exception {
         Socket cliSock;
 
-        try { sock = new ServerSocket(9999); }
-        catch(IOException ex) {
+        try{
+            sock = new ServerSocket(2807);
+            System.out.println("Server connection opened. Let's gooooooooooo!");
+        }
+        catch(IOException ex){
             System.out.println("Failed to open server socket");
             System.exit(1);
         }
 
-        while(true) {
+        while(true){
             cliSock=sock.accept();
             new Thread(new TcpSrvSumThread(cliSock)).start();
         }
@@ -34,27 +37,27 @@ class TcpSrvSumThread implements Runnable {
     private DataOutputStream sOut;
     private DataInputStream sIn;
 
-    public TcpSrvSumThread(Socket cli_s) { s=cli_s;}
+    public TcpSrvSumThread(Socket cli_s){
+        s=cli_s;
+    }
 
-    private final AutomaticallyAssignOrdersToFreeAGVController controller = new AutomaticallyAssignOrdersToFreeAGVController(); //-> used in US4002
+    //private final AutomaticallyAssignOrdersToFreeAGVController controller = new AutomaticallyAssignOrdersToFreeAGVController(); //-> used in US4002
 
     public void run() {
-        long f,i,num,sum;
+        //long f,i,num,sum;
         InetAddress clientIP;
 
         clientIP=s.getInetAddress();
         System.out.println("New client connection from " + clientIP.getHostAddress() +
                 ", port number " + s.getPort());
-        try {
+        try{
             sOut = new DataOutputStream(s.getOutputStream());
             sIn = new DataInputStream(s.getInputStream());
-
-            //preencher código necessário à US
 
             //==============================
             //====== INICIO DA US4002 ======
             //==============================
-            TheOrder selectedOrder;
+            /*TheOrder selectedOrder;
             AGV selectedAGV;
             List<TheOrder> ordersToAssign = controller.getOrdersToAssign();
             List<AGV> agvsAvailable = controller.getAGVsAvailable();
@@ -75,17 +78,19 @@ class TcpSrvSumThread implements Runnable {
                 controller.updateAGV(selectedAGV);
                 System.out.printf("AGV (ID: %d) successfully assigned to the Order (ID: %d). The Order (ID: %d) is now being prepared in the Warehouse!\n", selectedAGV.getAgvID(), selectedOrder.getOrderId(), selectedOrder.getOrderId());
                 number++;
-            } while (number+1<=ordersToAssignSize && number+1<=agvsAvailableSize);
+            } while (number+1<=ordersToAssignSize && number+1<=agvsAvailableSize);*/
             //=============================
             //======= FIM DA US4002 =======
             //=============================
 
+            System.out.println("I am client " + clientIP.getHostAddress() + " and I am finally connected to the server " + s.getLocalAddress() + "! :)");
 
             System.out.println("Client " + clientIP.getHostAddress() + ", port number: " + s.getPort() +
                     " disconnected");
             s.close();
+        }catch(IOException ex){
+            System.out.println("IOException");
         }
-        catch(IOException ex) { System.out.println("IOException"); }
     }
 }
 
