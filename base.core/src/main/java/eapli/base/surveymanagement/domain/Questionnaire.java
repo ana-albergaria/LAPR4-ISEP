356e64e2e064b300a9176c31553c73f52fb521db
martaribeiro <1201592@isep.ipp.pt>
2022-05-26 16:17:35 +0100

    Issue #21 - finished developing questionnaire.

--- base.core/src/main/java/eapli/base/surveymanagement/domain/Questionnaire.java
+++ base.core/src/main/java/eapli/base/surveymanagement/domain/Questionnaire.java
@@ -1,16 +1,17 @@
 package eapli.base.surveymanagement.domain;
 
-import eapli.base.productmanagement.domain.AlphaNumericCode;
-import eapli.base.productmanagement.domain.Code;
 import eapli.framework.domain.model.AggregateRoot;
 import eapli.framework.domain.model.DomainEntities;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
+/**
+ * A Questionnaire.
+ *
+ * @author Marta Ribeiro 1201592
+ */
 @Entity
 public class Questionnaire implements AggregateRoot<String>, Serializable {
 
@@ -27,22 +28,44 @@ public class Questionnaire implements AggregateRoot<String>, Serializable {
 
     private String welcomeMessage;
 
+    private String finalMessage;
+
     @ElementCollection
-    @CollectionTable(name = "sections", joinColumns = {@JoinColumn(name = "questionnaire_code", referencedColumnName = "id")})
+    @CollectionTable(name = "sections", joinColumns = {@JoinColumn(name = "questionnaire_code", referencedColumnName = "code")})
     @MapKeyColumn(name = "section")
     @Column(name = "question")
     private HashMap<String, String> sectionsAndQuestions = new HashMap<>();
 
-    public Questionnaire(final String code, final String title, final String welcomeMessage, final HashMap<String,String> sectionsAndQuestions){
+    public Questionnaire(final String code, final String title, final String welcomeMessage, final HashMap<String,String> sectionsAndQuestions, final String finalMessage){
         this.code=code;
         this.title=title;
         this.welcomeMessage=welcomeMessage;
         this.sectionsAndQuestions=sectionsAndQuestions;
-
+        this.finalMessage=finalMessage;
     }
 
     protected Questionnaire(){
 
+    }
+
+    public String code() {
+        return code;
+    }
+
+    public String title() {
+        return title;
+    }
+
+    public String welcomeMessage() {
+        return welcomeMessage;
+    }
+
+    public String finalMessage() {
+        return finalMessage;
+    }
+
+    public HashMap<String, String> sectionsAndQuestions() {
+        return sectionsAndQuestions;
     }
 
     @Override
@@ -57,7 +80,16 @@ public class Questionnaire implements AggregateRoot<String>, Serializable {
 
     @Override
     public boolean sameAs(Object other) {
-        throw new IllegalArgumentException("to develop");
+        if (!(other instanceof Questionnaire)) {
+            return false;
+        }
+
+        final var that = (Questionnaire) other;
+        if (this == that) {
+            return true;
+        }
+
+        return identity().equals(that.identity());
     }
 
     @Override
