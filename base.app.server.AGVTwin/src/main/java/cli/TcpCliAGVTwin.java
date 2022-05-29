966a1343e34f28452e1f8f79d2cec680c919f3b1
martaribeiro <1201592@isep.ipp.pt>
2022-05-29 19:46:49 +0100

    Issue #13 - Removed code from agv twin client.

--- base.app.server.AGVTwin/src/main/java/cli/TcpCliAGVTwin.java
+++ base.app.server.AGVTwin/src/main/java/cli/TcpCliAGVTwin.java
@@ -21,51 +21,6 @@ public class TcpCliAGVTwin {
     static InetAddress serverIP;
     static String address;
 
-    /*private static class ClientSocket {
-        private Socket sock;
-        private InetAddress serverIP;
-        private DataOutputStream sOutData;
-        private DataInputStream sInData;
-
-        public void connect(final String address, final int port) throws IOException {
-            try {
-                serverIP = InetAddress.getByName(address);
-            } catch (UnknownHostException ex) {
-                System.out.println("Invalid server specified: " + serverIP);
-                System.exit(1);
-            }
-
-            try {
-                sock = new Socket(serverIP, port);
-            } catch (IOException ex) {
-                System.out.println("Failed to establish TCP connection");
-                System.exit(1);
-            }
-
-            System.out.println("Connected to: " + serverIP + ":" + 2807);
-
-            sOutData = new DataOutputStream(sock.getOutputStream());
-            sInData = new DataInputStream(sock.getInputStream());
-        }
-
-        public DataOutputStream dataOutputStream() {
-            return this.sOutData;
-        }
-
-        public DataInputStream dataInputStream() {
-            return this.sInData;
-        }
-
-        public Socket sock() {
-            return this.sock;
-        }
-
-        public void stop() throws IOException {
-            sock.close();
-        }
-
-    }*/
-
     //client: request
     //server: waiting for request + send response
 
@@ -84,65 +39,7 @@ public class TcpCliAGVTwin {
 
     }
 
-    /*private DataOutputStream sOutData;
-    private DataInputStream sInData;*/
-
-
-    /*public void updateAgvsStatus(List<AGV> agvsToUpdate){
-        try{
-            final var socket = new ClientSocket();
-            socket.connect(getAddress(),getPort());
-            try{
-                if (MessageUtils.testCommunicationWithServer(socket.sOutData, socket.sInData)){
-
-                    for (AGV agv: agvsToUpdate
-                         ) {
-
-                    }
-
-                    MessageUtils.writeMessageWithData((byte) 8, socket.sOutData);
-
-                    byte[] clientMessageUS = new byte[4];
-                    MessageUtils.readMessage(clientMessageUS,socket.sInData);
-                    if (MessageUtils.wantsToExit(socket.sOutData,socket.sInData)) {
-                        socket.stop();
-                    } else {
-                        System.out.println("==> ERROR: Erro no pacote do Servidor");
-
-                    }
-
-                } else {
-                    System.out.println("==> ERROR: Erro no pacote do Servidor");
-                }
-            }catch (IOException e) {
-                System.out.println("==> ERROR: Falha durante a troca de informação com o server");
-            } finally {
-                try {
-                    socket.stop();
-                } catch (IOException e) {
-                    System.out.println("==> ERROR: Falha a fechar o socket");
-                }
-            }
-        }catch (Exception e) {
-            System.out.println("Server down");
-            System.out.println(e.getMessage());
-        }
-    }
-
-    private int getPort() {
-        // TODO read from config file
-        return 10000;
-    }
-
-    private String getAddress() {
-        // TODO read from config file
-        return "localhost";
-        //return "192.168.1.5"; -> ipv4 do terminal local windows
-    }*/
-
 }
-
-
 
 class TcpCliAGVTwinThread implements Runnable {
     private String ip;
