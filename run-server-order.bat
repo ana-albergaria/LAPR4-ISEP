8a7150b3da4a99d4765573e38d5d7c816a25bd99
Ana Albergaria <1201518@isep.ipp.pt>
2022-05-29 17:33:56 +0100

    Issue #16 - adding script for linux

--- run-server-order.bat
+++ run-server-order.bat
@@ -3,4 +3,4 @@ REM assumes the build was executed with maven copy-dependencies
 SET BASE_CP=base.app.server.order\target\base.app.server.order-1.4.0-SNAPSHOT.jar;base.app.server.order\target\dependency\*;
 
 REM call the java VM, e.g,
-java -cp %BASE_CP% eapli.base.app.server.order.tcp.TcpSrvOrder
+java -cp %BASE_CP% eapli.base.app.server.order.tcp.TcpOrderSrv
