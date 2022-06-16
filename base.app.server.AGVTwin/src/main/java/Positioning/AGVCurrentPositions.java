8a88f03b278e02117c5f44dbf8e8c98c7ebd4594
Diogo Violante <1201284@isep.ipp.pt>
2022-06-16 16:02:30 +0100

    issue #27 - implementing Positioning module for Digital Twin

--- base.app.server.AGVTwin/src/main/java/Positioning/AGVCurrentPositions.java
+++ base.app.server.AGVTwin/src/main/java/Positioning/AGVCurrentPositions.java
@@ -1,13 +1,58 @@
 package Positioning;
 
 import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.warehousemanagement.domain.AGV;
 import eapli.base.warehousemanagement.domain.AGVPosition;
+import eapli.base.warehousemanagement.domain.Square;
 import eapli.base.warehousemanagement.repositories.AgvPositionRepository;
 
-public class AGVCurrentPositions {
-    private final AgvPositionRepository repository = PersistenceContext.repositories().positions();
+import java.awt.geom.Point2D;
+import java.util.LinkedList;
 
-    public Iterable<AGVPosition> getPositions(){
-        return repository.findAll();
+public class AGVCurrentPositions {
+    private static Double speed;
+    private static String[][] warehousePlant;
+    private static AGV currentAGV;
+    private static LinkedList<Point2D> thisRoute;
+    private static int timeSpent;
+
+
+    public AGVCurrentPositions (Double currentSpeed, String[][] matrix, AGV agv, LinkedList<Point2D> route, int time) {
+        speed=currentSpeed;
+        warehousePlant=matrix;
+        currentAGV=agv;
+        thisRoute = route;
+        timeSpent=time;
+    }
+
+    public static synchronized AGVPosition calculatePosition() {
+        AGVPosition currentPosition=null;
+        int nextIndex = (int) (speed * timeSpent);
+        int index;
+        Point2D nextPos;
+        long wsquare, lsquare;
+
+
+        for(int i=0; i< warehousePlant.length; i++){
+            for (int j=0; j< warehousePlant[0].length; j++){
+                if (warehousePlant[i][j].equalsIgnoreCase(String.valueOf(currentAGV.getAgvID()))){
+                    index = thisRoute.indexOf(new Point2D.Double((long) j, (long) i));
+                    nextPos = thisRoute.get(index+nextIndex);
+                    wsquare = Double.valueOf(nextPos.getX()).longValue();
+                    lsquare = Double.valueOf(nextPos.getY()).longValue();
+                    currentPosition = new AGVPosition(new Square(wsquare, lsquare), currentAGV);
+                }
+            }
+        }
+
+        if(currentPosition==null){
+            index = thisRoute.indexOf(new Point2D.Double(0L, 0L));
+            nextPos = thisRoute.get(index+nextIndex);
+            wsquare = Double.valueOf(nextPos.getX()).longValue();
+            lsquare = Double.valueOf(nextPos.getY()).longValue();
+            currentPosition = new AGVPosition(new Square(wsquare, lsquare), currentAGV);
+        }
+
+        return currentPosition;
     }
 }
